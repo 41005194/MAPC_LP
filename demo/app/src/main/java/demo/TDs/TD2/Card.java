4a80212b447b2e27be9c120b425d2431c121226b
41005194 <41005194@parisnanterre.fr>
2025-09-18 12:26:15 +0200

    TD2 ex2

--- demo/app/src/main/java/demo/TDs/TD2/Card.java
+++ demo/app/src/main/java/demo/TDs/TD2/Card.java
@@ -1,11 +1,32 @@
 package demo.TDs.TD2;
 
 public class Card {
+
     public enum CardType {
         ACE_CLUBS, TWO_CLUBS, THREE_CLUBS, FOUR_CLUBS, FIVE_CLUBS, SIX_CLUBS, SEVEN_CLUBS, EIGHT_CLUBS, NINE_CLUBS, TEN_CLUBS, JACK_CLUBS, QUEEN_CLUBS, KING_CLUBS,
         ACE_DIAMONDS, TWO_DIAMONDS, THREE_DIAMONDS, FOUR_DIAMONDS, FIVE_DIAMONDS, SIX_DIAMONDS, SEVEN_DIAMONDS, EIGHT_DIAMONDS, NINE_DIAMONDS, TEN_DIAMONDS, JACK_DIAMONDS, QUEEN_DIAMONDS, KING_DIAMONDS,
         ACE_HEARTS, TWO_HEARTS, THREE_HEARTS, FOUR_HEARTS, FIVE_HEARTS, SIX_HEARTS, SEVEN_HEARTS, EIGHT_HEARTS, NINE_HEARTS, TEN_HEARTS, JACK_HEARTS, QUEEN_HEARTS, KING_HEARTS,
         ACE_SPADES, TWO_SPADES, THREE_SPADES, FOUR_SPADES, FIVE_SPADES, SIX_SPADES, SEVEN_SPADES, EIGHT_SPADES, NINE_SPADES, TEN_SPADES, JACK_SPADES, QUEEN_SPADES, KING_SPADES
+    }
+
+    public enum Rank {
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    }
+
+    public enum Suit {
+        CLUBS, DIAMONDS, SPADES, HEARTS;
+
+        public Color color() {
+            if (this == CLUBS || this == SPADES) {
+                return Color.BLACK;
+            } else {
+                return Color.RED;
+            }
+        }
+    }
+
+    public enum Color {
+        RED, BLACK
     }
 
     private CardType cardType;
@@ -24,15 +45,15 @@ public class Card {
      * Suit of a card.
      * @return the suit of the card.
      */
-    public String getSuit() {
+    public Suit getSuit() {
         if (cardType.ordinal() < 13) {
-            return "CLUBS";
+            return Suit.CLUBS;
         } else if (cardType.ordinal() < 26) {
-            return "DIAMONDS";
+            return Suit.DIAMONDS;
         } else if (cardType.ordinal() < 39) {
-            return "HEARTS";
+            return Suit.HEARTS;
         } else {
-            return "SPADES";
+            return Suit.SPADES;
         }
     }
 
@@ -40,17 +61,17 @@ public class Card {
      * Rank of a card.
      * @return the rank of the card.
      */
-    public String getRank() {
+    public Rank getRank() {
         if (cardType.ordinal() % 13 == 0) {
-            return "ACE";
+            return Rank.ACE;
         } else if (cardType.ordinal() % 13 == 10) {
-            return "JACK";
+            return Rank.JACK;
         } else if (cardType.ordinal() % 13 == 11) {
-            return "QUEEN";
+            return Rank.QUEEN;
         } else if (cardType.ordinal() % 13 == 12) {
-            return "KING";
+            return Rank.KING;
         } else {
-            return String.valueOf((cardType.ordinal() % 13) + 1);
+            return Rank.values()[cardType.ordinal() % 13];
         }
     }
 
--- demo/app/src/test/java/demo/TDs/TD2/CardTest.java
+++ demo/app/src/test/java/demo/TDs/TD2/CardTest.java
@@ -3,7 +3,7 @@ package demo.TDs.TD2;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CardTest {
+class CardTest {
     @Test
     void testCardTypeToString() {
         Card card = new Card(Card.CardType.ACE_CLUBS);
@@ -13,17 +13,29 @@ public class CardTest {
     @Test
     void testGetSuit() {
         Card card = new Card(Card.CardType.KING_DIAMONDS);
-        assertEquals("DIAMONDS", card.getSuit());
+        assertEquals(Card.Suit.DIAMONDS, card.getSuit());
     }
 
     @Test
     void testGetRank() {
         Card card = new Card(Card.CardType.TEN_SPADES);
-        assertEquals("10", card.getRank());
+        assertEquals(Card.Rank.TEN, card.getRank());
     }
 
     @Test
     void testNullCardTypeThrows() {
         assertThrows(AssertionError.class, () -> new Card(null));
+    }
+
+    @Test
+    void testGetColor() {
+        Card card1 = new Card(Card.CardType.ACE_CLUBS);
+        assertEquals(Card.Color.BLACK, card1.getSuit().color());
+        Card card2 = new Card(Card.CardType.KING_DIAMONDS);
+        assertEquals(Card.Color.RED, card2.getSuit().color());
+        Card card3 = new Card(Card.CardType.TEN_SPADES);
+        assertEquals(Card.Color.BLACK, card3.getSuit().color());
+        Card card4 = new Card(Card.CardType.QUEEN_HEARTS);
+        assertEquals(Card.Color.RED, card4.getSuit().color());
     }
 }
